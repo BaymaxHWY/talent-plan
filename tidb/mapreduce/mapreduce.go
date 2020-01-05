eb68da5823066669abedd3432acb36a1df268f08
BaymaxHWY <854541526@qq.com>
2020-01-05 23:28:30 +0800

    完成mapreduce.go

--- tidb/mapreduce/mapreduce.go
+++ tidb/mapreduce/mapreduce.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"encoding/json"
+	"fmt"
 	"hash/fnv"
 	"io/ioutil"
 	"log"
@@ -112,7 +113,44 @@ func (c *MRCluster) worker() {
 				// hint: don't encode results returned by ReduceF, and just output
 				// them into the destination file directly so that users can get
 				// results formatted as what they want.
-				panic("YOUR CODE HERE")
+
+				// 1. 读取中间文件--需要获得文件名，再进行读取
+				// 2. 对其中的kv键值对进行排序--sort库函数
+				// 3. 对每个key调用reduceF--需要一个排序好了的valueslice
+				// 4. 将reduceF的输出写入磁盘--已经给出了方法
+				mapFiles := make([]*os.File, t.nMap)
+				mapFileBuffers := make([]*bufio.Reader, t.nMap)
+				contents := make([]*json.Decoder, t.nMap)
+				// 获得文件句柄和decoder数组
+				for i := 0; i < t.nMap; i++ {
+					mapFilePath := reduceName(t.dataDir, t.jobName, i, t.taskNumber)
+					mapFiles[i], mapFileBuffers[i] = OpenFileAndBuf(mapFilePath)
+					contents[i] = json.NewDecoder(mapFileBuffers[i])
+				}
+				// 读取文件内容
+				// 将相同的键值的value存储到一起
+				kvList := make(map[string][]string)
+				for i := 0; i < t.nMap; i++ {
+					for { // 需要持读取文件
+						var kv KeyValue
+						err := contents[i].Decode(&kv)
+						if err != nil {
+							if err.Error() != "EOF" {
+								fmt.Printf("Failed to read from REDUCE input file %s: %v\n", mapFiles[i].Name(), err)
+							}
+							break
+						}
+						kvList[kv.Key] = append(kvList[kv.Key], kv.Value)
+					}
+				}
+				// 对 Key 进行排序，数据量太大（无法读入内存时，才进行排序）
+				outFile := mergeName(t.dataDir, t.jobName, t.taskNumber)
+				out, outBuffer := CreateFileAndBuf(outFile)
+				for k, v := range kvList {
+					reduced := t.reduceF(k, v)
+					WriteToBuf(outBuffer, reduced)
+				}
+				SafeClose(out, outBuffer)
 			}
 			t.wg.Done()
 		case <-c.exit:
@@ -159,7 +197,32 @@ func (c *MRCluster) run(jobName, dataDir string, mapF MapF, reduceF ReduceF, map
 
 	// reduce phase
 	// YOUR CODE HERE :D
-	panic("YOUR CODE HERE")
+	// 并发向worker发送reduce task
+	tasks = make([]*task, 0, nReduce)
+	for i := 0; i < nReduce; i++ {
+		t := &task{
+			dataDir:    dataDir,
+			jobName:    jobName,
+			mapFile:    "",
+			phase:      reducePhase,
+			taskNumber: i,
+			nMap:       nMap,
+			nReduce:    nReduce,
+			mapF:       nil,
+			reduceF:    reduceF,
+			wg:         sync.WaitGroup{},
+		}
+		t.wg.Add(1)
+		tasks = append(tasks, t)
+		go func() {c.taskCh <- t}() // 防止死锁
+	}
+	outFiles := make([]string, 0, nReduce)
+	for _, t := range tasks {
+		t.wg.Wait() // 等处理完
+		file := mergeName(t.dataDir, t.jobName, t.taskNumber)
+		outFiles = append(outFiles, file)
+	}
+	notify <- outFiles
 }
 
 func ihash(s string) int {
